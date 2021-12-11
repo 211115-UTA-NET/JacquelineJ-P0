97ac4ef5621be46b5081642f94a380e24c204f39
Jacqueline Jabraj <jjabraj2227@gmail.com>
2021-12-11 16:52:01 -0500

    first commit of Project0

---
+++ ProjectP0/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectP0
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string CustomerFirstName { get; set; }
+        
+        public string CustomerLastName { get; set; }
+           
+    }
+}
+
---
+++ ProjectP0/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ProjectP0
+{
+    public class Program
+    {
+       public static void Main(string[] args)
+        {
+            Customer customerObj = new Customer();
+            Console.WriteLine("Enter Customer First Name :");
+            customerObj.CustomerFirstName = Console.ReadLine();
+            Console.WriteLine("Enter Customer Last Name : ");
+            customerObj.CustomerLastName = Console.ReadLine();
+            Console.WriteLine($"Given Customer First and Last Names are : {customerObj.CustomerFirstName} {customerObj.CustomerLastName} ");
+            Console.ReadKey();
+      
+        }
+    }
+}
